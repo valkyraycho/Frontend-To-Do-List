8797e02dbfa88625e588432b0532dbe9e7ff5fe3
Ray Cho <razor0424@gmail.com>
2023-12-08 19:38:51 -0800

    add animation

--- js/script.js
+++ js/script.js
@@ -18,16 +18,19 @@ const initApp = () => {
 
   const clearItems = document.getElementById("clearItems");
   clearItems.addEventListener("click", (event) => {
-    console.log("clicked");
+    let items = document.getElementsByClassName("item");
+
+    Array.from(items).forEach(function (item) {
+      item.querySelector("input[type='checkbox']").checked = true;
+    });
+
     const list = toDoList.getList();
-    if (list.length) {
-      const confirmed = confirm("Are you sure you want to clear?");
-      if (confirmed) {
-        toDoList.clearList();
-        updatePersistentData(toDoList.getList());
-        refreshPage();
-      }
-    }
+
+    toDoList.clearList();
+    updatePersistentData(toDoList.getList());
+    setTimeout(() => {
+      refreshPage();
+    }, 500);
   });
 
   loadListObject();
